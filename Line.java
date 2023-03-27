38d9a790a6aa8fa5ceb0cac933be47f924a29b3a
unknown <BT_3W2_23@BKTECH.LOCAL>
2023-03-27 12:32:07 -0400

    Initial commit

---
+++ Line.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+
+public class Line extends Shape {
+
+    public Line() {
+        super();
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(getColor());
+        g.drawLine(getP1().x, getP1().y, getP2().x, getP2().y);
+    }
+}
